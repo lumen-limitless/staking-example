f56def05e5eda457c5c49b69ca9ff266d6c9c5d2
lumen-limitless <lumenlimitless@gmail.com>
2023-01-25 20:19:56 -0500

    hotfix

--- staking-example-app/src/components/Nav.tsx
+++ staking-example-app/src/components/Nav.tsx
@@ -3,8 +3,8 @@ import { Web3Button, Web3NetworkSwitch } from '@web3modal/react'
 export default function Nav() {
   return (
     <div className="flex h-16 w-full items-center px-4 md:px-12 lg:px-36">
+      <h1>Staking Example dApp</h1>
       <div className="ml-auto flex  items-center justify-center gap-1">
-        <Web3NetworkSwitch />
         <Web3Button />
       </div>
     </div>
